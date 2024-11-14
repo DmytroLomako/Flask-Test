de9994e0aff6fc71edcb0a328e62fb3a44f0cab0
DmytroLomako <lomakodmytro@gmail.com>
2024-11-14 19:05:36 +0200

    work with styles

--- home_app/views.py
+++ home_app/views.py
@@ -1,4 +1,8 @@
 import flask, reg_app
 
 def render_home():
+    if flask.request.method == 'POST':
+        email = flask.request.form['email']
+        message = flask.request.form['message']
+        print(email, message)
     return flask.render_template('home_app/home.html', list = reg_app.list_users)
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
 import project
 
 if __name__ == '__main__':
-    project.project_shop.run(debug = True)    
+    project.project_shop.run('127.0.0.1', 8000, debug = True)    
