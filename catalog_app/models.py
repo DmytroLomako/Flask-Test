6d6b6e8bac1eed3d663c5b2670c8ec1ddf889d9f
DmytroLomako <lomakodmytro@gmail.com>
2024-12-09 17:28:39 +0200

    work with db images

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -5,6 +5,12 @@ class Product(database.Model):
     name = database.Column(database.String)
     price = database.Column(database.Integer)
     description = database.Column(database.String)
-    # image_path = database.Column(database.String)
+    image = database.Column(database.String)
+    def __repr__(self):
+        return self.name
+    
+class Order(database.Model):
+    id = database.Column(database.Integer, primary_key = True)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'))
     def __repr__(self):
         return self.name
--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,13 +1,29 @@
-import flask
-# from .models import Product
+import flask, os
+from .models import Product
+from project.settings import database
 
 def render_catalog():
     if flask.request.method == 'POST':
-        name = flask.request.form['name']
-        price = flask.request.form['price']
-        description = flask.request.form['description']
-        print(name, price, description)
-    list_products = []#Product.query.all()
+        if 'id-product' in flask.request.form:
+            product_id = flask.request.form['id-product']
+            product = Product.query.get(product_id)
+            path_to_delete = os.path.abspath(__file__ + f'/../static/catalog_app/img/products/{product.image}.png')
+            os.remove(path_to_delete)
+            database.session.delete(product)
+            database.session.commit()
+        else:
+            name = flask.request.form['name']
+            price = flask.request.form['price']
+            description = flask.request.form['description']
+            product = Product(name = name, price = price, description = description)
+            database.session.add(product)
+            database.session.commit()
+            product.image = f'{name}-{product.id}'
+            database.session.commit()
+            image = flask.request.files['image']
+            image_path = os.path.abspath(__file__ + f'/../static/catalog_app/img/products/{product.image}.png')
+            image.save(image_path)
+    list_products = Product.query.all()
     return flask.render_template('catalog_app/catalog.html', products = list_products)
 
 def render_product():
